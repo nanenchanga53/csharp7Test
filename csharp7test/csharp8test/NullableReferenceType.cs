181915e78c35fe25a9e6942e7560e80dc95a203f
nanenchanga53 <nanenchanga53@gmail.com>
2020-10-15 13:34:52 +0900

    8.0 Nullable Reference Type
    8.0에서는 기본 null로 선언하거나 값을 넣었을 경우 warning을 표시할 수 있도록 설정할 수 있다

---
+++ csharp7test/csharp8test/NullableReferenceType.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp8test
+{
+        public class NullableReferenceType
+        {
+                //C# 8.0 에서는 Reference 타입에 NULL을 할당하면 컴파일러가 경고(Warning)를 주는 기능을 추가하였다.
+                //즉, C# 8.0에서 (특별한 옵션을 설정하면) Reference 타입은 기본적으로 NULL을 넣을 수 없는 Non-nullable Reference Type이 되고, 
+                //NULL을 허용하기 위해서는 string? 와 같이 레퍼런스 타입 뒤에 물음표(?)를 붙여 Nullable Reference Type임을 표시하게 된다
+                public NullableReferenceType()
+                {
+                        Console.WriteLine(@"
+                        C# 프로젝트 파일(*.csproj)에서 <Nullable>enable</Nullable> 을 
+                        /Project/PropertyGroup 안에 넣으면, 프로젝트 레벨에서 Nullable Reference Type 기능을 사용하게 된다. 
+                        또한, 소스 파일의 첫 라인에 #nullable enable 을 사용하면 파일 레벨에서 Nullable Reference Type 기능을 사용하게 된다. 
+                        마찬가지로 소스 코드 중간에 #nullable enable을 넣으면 그 다음 라인부터 Nullable Reference 기능이 활성화 되고, 
+                        #nullable disable을 넣으면 그 다음 라인부터 이 기능이 비활성화 된다.
+                        ");
+
+                        NoUseNullableReferenceType();
+                        Console.WriteLine(@"
+                        private void NoUseNullableReferenceType()
+                        {
+                                #nullable enable
+                                string s1 = null; // Warning: Converting null literal or 
+                                                  // possible null value to non-nullable type
+                                if (s1 == null) return;
+
+                                string? s2 = null;
+                                if (s2 == null) return;
+
+                                #nullable disable
+                                string s3 = null; // No Warning
+                                if (s3 == null) return;
+                        }");
+
+
+                        Console.WriteLine(@"
+                        Nullable Reference Type은 기존 Reference Type 뒤에 물음표를 붙여 표시한다. 
+                        예를 들어, string은 Non-nullable Reference Type이며, 
+                        string? 은 Nullable Reference Type임을 표시한다. 
+                        Nullable Reference Type의 변수를 사용할 때는 항상 먼저 NULL 인지를 체크해야 하며, 
+                        만약 그렇지 않으면 컴파일러가 경고(Warning) 메시지를 표시한다");
+
+                        Console.WriteLine(@"
+                        private void UseNullableReferenceType(string? s)
+                        {
+                                Console.WriteLine(s.Length); // Warning: Dereference of a possibly null reference
+
+                                if (s != null)
+                                {
+                                        Console.WriteLine(s);
+                                }
+                                else
+                                {
+                                        Console.WriteLine('Null값');
+                                }
+                        }");
+                        UseNullableReferenceType(null);
+                        UseNullableReferenceType("null이 아닌 값");
+                }
+
+                /// <summary>
+                /// 널값이면 Warning을 방생하는 설정과 사용하지 않는 설정을 보자
+                /// </summary>
+                private void NoUseNullableReferenceType()
+                {
+                        #nullable enable
+                        string s1 = null; // Warning: Converting null literal or 
+                                          // possible null value to non-nullable type
+                        if (s1 == null) return;
+
+                        string? s2 = null;
+                        if (s2 == null) return;
+
+                        #nullable disable
+                        string s3 = null; // No Warning
+                        if (s3 == null) return;
+                }
+
+                #nullable enable
+                /// <summary>
+                /// Null값을 받아올 수 있는 설정이면 앞에 ?를 붙인다
+                /// </summary>
+                /// <param name="s"></param>
+                private void UseNullableReferenceType(string? s)
+                {
+                        Console.WriteLine(s.Length); // Warning: Dereference of a possibly null reference
+
+                        if (s != null)
+                        {
+                                Console.WriteLine(s);
+                        }
+                        else
+                        {
+                                Console.WriteLine("Null값");
+                        }
+                }
+        }
+}
--- csharp7test/csharp8test/Program.cs
+++ csharp7test/csharp8test/Program.cs
@@ -4,81 +4,81 @@ using static System.Convert;
 
 namespace csharp8test
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            WriteLine("새 기능들 리스트 중 하나를 고르시오(참조사이트 http://www.csharpstudy.com/latest/CS-new-features.aspx)");
-            WriteLine("C# 8.0을 사용하기 위해서는 Visual Studio 2019과 .NET Core 3.0이상 필요");
-            WriteLine("---------------------------------------------------------------------------------------------------------");
-            WriteLine("1.디폴트 인터페이스 멤버 (Default Inteface Members)");
-            WriteLine("2.향상된 패턴 매칭 기능 (Pattern Matching)");
-            WriteLine("3.Nullable Reference Type");
-            WriteLine("4.인덱싱과 슬라이싱 (Indexing / Slicing)");
-            WriteLine("5.비동기 스트림 (Async Stream)");
-            WriteLine("6.using 선언");
-            WriteLine("7.널 병합 할당 연산자 (Null-coalescing assignment)");
-            WriteLine("8.구조체(struct) 읽기 전용 멤버");
-            WriteLine("9.기타 기능들");
+                static void Main(string[] args)
+                {
+                        WriteLine("새 기능들 리스트 중 하나를 고르시오(참조사이트 http://www.csharpstudy.com/latest/CS-new-features.aspx)");
+                        WriteLine("C# 8.0을 사용하기 위해서는 Visual Studio 2019과 .NET Core 3.0이상 필요");
+                        WriteLine("---------------------------------------------------------------------------------------------------------");
+                        WriteLine("1.디폴트 인터페이스 멤버 (Default Inteface Members)");
+                        WriteLine("2.향상된 패턴 매칭 기능 (Pattern Matching)");
+                        WriteLine("3.Nullable Reference Type");
+                        WriteLine("4.인덱싱과 슬라이싱 (Indexing / Slicing)");
+                        WriteLine("5.비동기 스트림 (Async Stream)");
+                        WriteLine("6.using 선언");
+                        WriteLine("7.널 병합 할당 연산자 (Null-coalescing assignment)");
+                        WriteLine("8.구조체(struct) 읽기 전용 멤버");
+                        WriteLine("9.기타 기능들");
 
-            int selectedNum = ToInt32(ReadLine());
+                        int selectedNum = ToInt32(ReadLine());
 
-            switch(selectedNum)
-            {
-                case 1:
-                    WriteLine("1.디폴트 인터페이스 멤버 (Default Inteface Members)");
-                    WriteLine("Selected");
+                        switch (selectedNum)
+                        {
+                                case 1:
+                                        WriteLine("1.디폴트 인터페이스 멤버 (Default Inteface Members)");
+                                        WriteLine("Selected");
 
-                    DefaultInterfaceMembers dim = new DefaultInterfaceMembers();
-                    
-                    
-                    break;
-                case 2:
-                    WriteLine("2.향상된 패턴 매칭 기능 (Pattern Matching)");
-                    WriteLine("Selected");
+                                        DefaultInterfaceMembers dim = new DefaultInterfaceMembers();
 
-                    PatternMatching ptm = new PatternMatching();
+                                        break;
+                                case 2:
+                                        WriteLine("2.향상된 패턴 매칭 기능 (Pattern Matching)");
+                                        WriteLine("Selected");
 
-                    break;
-                case 3:
-                    WriteLine("3.Nullable Reference Type");
-                    WriteLine("Selected");
+                                        PatternMatching ptm = new PatternMatching();
 
-                    break;
-                case 4:
-                    WriteLine("4.인덱싱과 슬라이싱 (Indexing / Slicing)");
-                    WriteLine("Selected");
+                                        break;
+                                case 3:
+                                        WriteLine("3.Nullable Reference Type");
+                                        WriteLine("Selected");
+                                        NullableReferenceType nrt = new NullableReferenceType();
 
-                    break;
-                case 5:
-                    WriteLine("5.비동기 스트림 (Async Stream)");
-                    WriteLine("Selected");
+                                        break;
+                                case 4:
+                                        WriteLine("4.인덱싱과 슬라이싱 (Indexing / Slicing)");
+                                        WriteLine("Selected");
 
-                    break;
-                case 6:
-                    WriteLine("6.using 선언");
-                    WriteLine("Selected");
+                                        break;
+                                case 5:
+                                        WriteLine("5.비동기 스트림 (Async Stream)");
+                                        WriteLine("Selected");
 
-                    break;
-                case 7:
-                    WriteLine("7.널 병합 할당 연산자 (Null-coalescing assignment)");
-                    WriteLine("Selected");
+                                        break;
+                                case 6:
+                                        WriteLine("6.using 선언");
+                                        WriteLine("Selected");
 
-                    break;
-                case 8:
-                    WriteLine("8.구조체(struct) 읽기 전용 멤버");
-                    WriteLine("Selected");
+                                        break;
+                                case 7:
+                                        WriteLine("7.널 병합 할당 연산자 (Null-coalescing assignment)");
+                                        WriteLine("Selected");
 
-                    break;
-                case 9:
-                    WriteLine("9.기타 기능들");
-                    WriteLine("Selected");
+                                        break;
+                                case 8:
+                                        WriteLine("8.구조체(struct) 읽기 전용 멤버");
+                                        WriteLine("Selected");
 
-                    break;
+                                        break;
+                                case 9:
+                                        WriteLine("9.기타 기능들");
+                                        WriteLine("Selected");
 
-            }
-            
+                                        break;
 
+                        }
+
+
+                }
         }
-    }
 }
